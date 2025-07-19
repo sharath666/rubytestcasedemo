ac765dcc7fa345d84ed6b940e22a546dddbda710
sharath <anzaserviesllp.com>
2025-07-19 15:26:49 +0530

    empty string check is done

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -1,4 +1,11 @@
 require 'minitest/autorun'
 require_relative 'string_calculator'
 class StringCalculatorTest < Minitest::Test
+  def setup
+    @calc = StringCalculator.new
+  end
+
+  def test_empty_string_returns_zero
+    assert_equal 0, @calc.add("")
+  end
 end
