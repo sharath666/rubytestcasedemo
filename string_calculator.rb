a9505073cb8139a6d9d7389d7b7e0f351ef3254d
sharath <anzaserviesllp.com>
2025-07-19 15:17:39 +0530

    completed the input method for all the possible cases

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,4 +1,17 @@
 class StringCalculator
     def add(input)
+      return 0 if input.nil? || input.strip.empty?
+      delimiter = /,|\n/
+      numbers_section = input
+
+      # Check for custom delimiter
+      if input.start_with?("//")
+        header, numbers_section = input.split("\n", 2)
+        delimiter = Regexp.escape(header[2]) # single-char delimiter
+      end
+      numbers = numbers_section.split(/#{delimiter}/).map(&:to_i)
+      negatives = numbers.select { |n| n < 0 }
+      raise "negative numbers not allowed: #{negatives.join(',')}" unless negatives.empty?
+      numbers.sum
     end
 end
